1850110f7e4d199af2706a15ed3839b5fff25ab0
TormodNy <tormodn99@gmail.com>
2022-10-01 18:07:34 +0200

    feature: add profile picture to posts

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,15 @@
 function Post ({ post }) {
   return (
-    <div className="flex flex-col p-2 bg-slate-100 text-left gap-4">
-      <h3>{post.title}</h3>
-      <p className="whitespace-normal">{post.body}</p>
-      <i className="font-light">Posted by {post.user.login} on {post.created_at}</i>
+    <div className="flex bg-slate-100 p-4 gap-2">
+      <div className="min-w-[48px]">
+          <img src={post.user.avatar_url} className="rounded-full w-12" />
+        </div>
+      <div className="flex flex-col p-2 text-left gap-4">
+    
+        <h3>{post.title}</h3>
+        <p className="whitespace-normal">{post.body}</p>
+        <i className="font-light text-sm">Posted by {post.user.login} on {post.created_at}</i>
+      </div>
     </div>
   );
 }
