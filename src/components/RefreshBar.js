8f634ce630d4e574eeb8dceec8506d33c5f7b9ab
TormodNy <tormodn99@gmail.com>
2022-10-02 14:04:10 +0200

    feature: add refresh button

---
+++ src/components/RefreshBar.js
@@ -0,0 +1,19 @@
+import { Button } from "@mui/material";
+import RefreshIcon from "@mui/icons-material/Refresh";
+
+function RefreshBar ({ setPosts }) {
+  function refresh () {
+    setPosts(null);
+  }
+
+  return (
+    <div className="w-full sticky top-4">
+      <Button variant="contained" fullWidth onClick={refresh}>
+        <RefreshIcon className="mr-2" />
+        Refresh
+      </Button>
+    </div>
+  );
+}
+
+export default RefreshBar;
