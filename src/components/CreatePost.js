cfda0c0ff4a02db948427aad7d434fc19cb6d27a
TormodNy <tormodn99@gmail.com>
2022-10-02 13:30:31 +0200

    feature: add character limit

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -4,6 +4,9 @@ import { postPost } from "../api/Posts";
 import { UserContext } from "../App";
 
 function CreatePost () {
+  const titleCharLimit = 60;
+  const bodyCharLimit = 1000;
+
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
 
@@ -15,11 +18,34 @@ function CreatePost () {
     setBody("");
   }
 
+  function updateTitle (e) {
+    setTitle(e.target.value.substring(0, titleCharLimit));
+  }
+
+  function updateBody (e) {
+    setBody(e.target.value.substring(0, bodyCharLimit));
+  }
+
   return (
     <div className="flex flex-col gap-2 pt-1 p-4 bg-slate-100 rounded-md">
       <h2>Create a post</h2>
-      <TextField label="Title" value={title} onChange={e => setTitle(e.target.value)} InputProps={{className: "bg-white"}}></TextField>
-      <TextField multiline rows={4} label="Body" value={body} onChange={e => setBody(e.target.value)} InputProps={{className: "bg-white"}}></TextField>
+      <TextField
+        label="Title"
+        value={title}
+        onChange={updateTitle}
+        InputProps={{className: "bg-white"}}
+        helperText={titleCharLimit - title.length + " characters remaining"}
+      />
+      <TextField
+        multiline
+        minRows={4}
+        maxRows={10}
+        label="Body"
+        value={body}
+        onChange={updateBody}
+        InputProps={{className: "bg-white"}}
+        helperText={bodyCharLimit - body.length + " characters remaining"}
+      />
       <Button variant="contained" onClick={publishPost}>Publish</Button>
     </div>
   );
